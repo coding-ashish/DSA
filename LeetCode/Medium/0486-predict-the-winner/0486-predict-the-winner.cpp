f9802e1a7c61a833277caf0274aaeb7abac278ba
Ashish Upadhyay <ashish_ug_24@ee.nits.ac.in>
2026-08-03 00:47:44 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16.32%) - LeetHub

--- LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.cpp
+++ LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,27 +2,33 @@ class Solution {
 public:
     bool predictTheWinner(vector<int>& piles) {
         // int s1 = score1()
-        int scr = score(piles,0,piles.size()-1,true);
+        vector<vector<int>> dp(piles.size(),vector<int>(piles.size(),-1));
+        // dp.assign(piles.size()-1,vector<int>(piles.size()-1,-1));
+        dp[0][piles.size()-1] = score(piles,0,piles.size()-1,true,dp);
 
         int sum = 0;
         for (int i = 0; i < piles.size(); i++) {
             sum += piles[i];
         }
 
-        return scr >= (sum - scr);
+        return dp[0][piles.size()-1] >= (sum - dp[0][piles.size()-1]);
     }
 
-    int score(vector<int>& piles, int i, int j, bool scor) {
+    int score(vector<int>& piles, int i, int j, bool scor,vector<vector<int>> &dp) {
         if (!scor) {
-            return min(score(piles, i + 1, j, true),
-                       score(piles, i, j - 1, true));
+            return min(score(piles, i + 1, j, true,dp),
+                       score(piles, i, j - 1, true,dp));
         }
 
         if (i > j) {
             return 0;
         }
 
-        return max(piles[i] + score(piles, i + 1, j, false),
-                   piles[j] + score(piles, i, j - 1, false));
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        return dp[i][j] = max(piles[i] + score(piles, i + 1, j, false,dp),
+                   piles[j] + score(piles, i, j - 1, false,dp));
     }
 };
